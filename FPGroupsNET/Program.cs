161d0bc2b1efccab37aa54c9ea0991ca232148de
Jo. H <44933755+p4jo@users.noreply.github.com>
2024-03-26 15:08:20 +0100

    add gap wrapper (server)

--- FPGroupsNET/Program.cs
+++ FPGroupsNET/Program.cs
@@ -1,27 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿int vertexCount = 5;    
+int edgeCount = 12;
+double proportionOfGenerators = 0.5;
 while (true)
 {
     
-    // read the parameters vertexCount, edgeCount from the command line, retrying if it couln't be converted to int
-    int vertexCount = 5;    
-    int edgeCount = 12;
-    double proportionOfGenerators = 0.5;
     while (true)
     {
-        Console.WriteLine("Enter the number of vertices: ");
+        Console.WriteLine($"Enter the number of vertices: [{vertexCount}]");
         string vertexCountString = Console.ReadLine() ?? string.Empty;
-        Console.WriteLine("Enter the number of edges: ");
+        Console.WriteLine($"Enter the number of edges: [{edgeCount}]");
         string edgeCountString = Console.ReadLine() ?? string.Empty;
-        Console.WriteLine("Enter the proportion of Generators (i.e. #Generators = max. Degree of graph * this; >= 0.5): ");
+        Console.WriteLine($"Enter the proportion of Generators (i.e. #Generators = max. Degree of graph * this; >= 0.5): [{proportionOfGenerators}]");
         string proportionOfGeneratorsString = Console.ReadLine() ?? string.Empty;
-        if (int.TryParse(vertexCountString, out vertexCount) && int.TryParse(edgeCountString, out edgeCount) && double.TryParse(proportionOfGeneratorsString?.Replace('.',','), out proportionOfGenerators))
-            break;
-        Console.WriteLine("Invalid input. Please enter a valid integer.");
+        if (
+            ( string.IsNullOrWhiteSpace(vertexCountString) || int.TryParse(vertexCountString, out vertexCount)   )
+            && 
+            ( string.IsNullOrWhiteSpace(edgeCountString) || int.TryParse(edgeCountString, out edgeCount) )
+            &&
+            ( string.IsNullOrWhiteSpace(proportionOfGeneratorsString) || double.TryParse(proportionOfGeneratorsString?.Replace('.',','), out proportionOfGenerators)) 
+        )  break;
+        Console.WriteLine("Invalid input. Please enter valid numbers.");
     }
 
+    if (edgeCount > 30)
+    {
+        Console.WriteLine("This might crash the program. We don't recommend more than 30 edges. Continue? y/n");
+        if (Console.ReadLine()?.ToLower() != "y") continue;
+    }
 
     var (gens, rels) = RandomGroups.RandomPresentation(vertexCount, edgeCount, proportionOfGenerators) ;
-    Console.WriteLine("<" + string.Join(", ", gens) + " | " + string.Join(", ", rels) + ">");
+    if (rels.Length > 1000)
+    {
+        Console.WriteLine($"There are {rels.Length} relators. Reducing them might not work. Continue? y/n");
+        if (Console.ReadLine()?.ToLower() != "y") continue;
+    }
+    Console.WriteLine("Unreduced group:\n<" + string.Join(", ", gens) + " | " + string.Join(", ", rels) + ">");
+    var (optGens, optRels, optMap) = await FpGroups.OptimizePresentation(gens, rels);
+    Console.WriteLine("Reduced group:\n<" + string.Join(", ", optGens) + " | " + string.Join(", ", optRels) + ">\nwith " + string.Join(", ",
+        from g in gens select g + " -> " + optMap[g]
+    ));
 }
